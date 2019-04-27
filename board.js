d27c454a867fa9441da47da0f29d0f35c79faf86
Alesh Houdek <mail@alesh.com>
2019-04-27 08:25:45 -0600

    Board functions in separate file

---
+++ board.js
@@ -0,0 +1,25 @@
+
+const makeBoard = () => {
+  const $board = $('<table>')
+  for (let i = 0; i < boardHeight; i++) {
+    $board.append(makeRow(i))
+  }
+  return $board
+}
+
+const makeRow = (rowId) => {
+  const $row = $('<tr>')
+  for (let j = 0; j < boardWidth; j++) {
+    const id = cellId(rowId, j)
+    $row.append(makeCell(id))
+  }
+  return $row
+}
+
+const makeCell = (id) => $('<td>').attr("id", id)
+
+const cellId = (row, cell) => {
+  let id =  (row > 9) ? "" + row : "0" + row
+  id += (cell > 9) ? cell : "0" + cell
+  return id
+}
--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-const boardHeight = 24
+'use strict'
+
 const boardWidth = 10
+const boardHeight = 24
 
 const init = () => {
   $('body').append(makeBoard())
-}
-
-const makeBoard = () => {
-  const $board = $('<table>')
-  for (let i = 0; i < boardHeight; i++) {
-    $board.append(makeRow(i))
-  }
-  return $board
-}
-
-const makeRow = (rowId) => {
-  const $row = $('<tr>')
-  for (let j = 0; j < boardWidth; j++) {
-    const id = cellId(rowId, j)
-    $row.append(makeCell(id))
-  }
-  return $row
-}
-
-const makeCell = (id) => $('<td>').attr("id", id)
-
-const cellId = (row, cell) => {
-  let id =  (row > 9) ? "" + row : "0" + row
-  id += (cell > 9) ? cell : "0" + cell
-  return id
 }
 
 init()
