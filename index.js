f87efd98347377d1fd324a540a525bb4049b4769
Alesh Houdek <mail@alesh.com>
2019-04-27 03:15:17 -0600

    Separate css and js files

---
+++ index.js
@@ -0,0 +1,33 @@
+const boardHeight = 24
+const boardWidth = 10
+
+const init = () => {
+  $('body').append(makeBoard())
+}
+
+const makeBoard = () => {
+  const $board = $('<table>')
+  for (let i = 0; i < boardHeight; i++) {
+    $board.append(makeRow(i))
+  }
+  return $board
+}
+
+const makeRow = (rowId) => {
+  const $row = $('<tr>')
+  for (let j = 0; j < boardWidth; j++) {
+    const id = cellId(rowId, j)
+    $row.append(makeCell(id))
+  }
+  return $row
+}
+
+const makeCell = (id) => $('<td>').attr("id", id)
+
+const cellId = (row, cell) => {
+  let id =  (row > 9) ? "" + row : "0" + row
+  id += (cell > 9) ? cell : "0" + cell
+  return id
+}
+
+init()
