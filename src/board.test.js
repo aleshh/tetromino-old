aae4daf134a4fa631f5f58c9f85f980d64303ce7
Alesh Houdek <mail@alesh.com>
2019-04-29 06:42:36 -0600

    Test that board has correct number of cells

--- src/board.test.js
+++ src/board.test.js
@@ -1,5 +1,10 @@
 import Board from './board'
+import config from './config'
 
-test('Builds board', () => {
-  Board.create()
+test('Builds board with correct number of cells', () => {
+  const expectedNumberOfCells = config.boardHeight * config.boardWidth
+  const $board = Board.create()
+  const $tableRows = $board.children()
+  const $tableCells = $tableRows.children()
+  expect($tableCells.length).toBe(expectedNumberOfCells)
 })
