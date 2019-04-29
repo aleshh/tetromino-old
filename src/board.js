143910cd6bc95fc1a6d58e56d418aa3a5b59dd8d
Alesh Houdek <mail@alesh.com>
2019-04-29 06:15:15 -0600

    Configured jest to use jquery

--- src/board.js
+++ src/board.js
@@ -1,4 +1,5 @@
 import config from './config.js'
+import $ from 'jquery'
 
 const Board = (function() {
 
--- src/board.test.js
+++ src/board.test.js
@@ -1,5 +1,5 @@
 import Board from './board'
 
 test('Builds board', () => {
-  // Board.create()
+  Board.create()
 })
