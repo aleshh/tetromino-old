4445894502f9fa1ca9c72a1b1c99f7b7d3d25a45
Alesh Houdek <mail@alesh.com>
2019-04-29 09:35:20 -0600

    Refactored board tests

--- src/board.test.js
+++ src/board.test.js
@@ -5,19 +5,21 @@ const $board = Board.create()
 const $tableRows = $board.children()
 const $tableCells = $tableRows.children()
 
-test('Builds board with correct number of cells', () => {
-  const expectedNumberOfCells = config.boardHeight * config.boardWidth
-  expect($tableCells.length).toBe(expectedNumberOfCells)
-})
-test('first cell has correct ID', () => {
-  const firstCellId = $tableCells.first().attr('id')
-  expect(firstCellId).toEqual('0000')
-})
-test('last cell has correct ID', () => {
-  const lastCellRow = "0" + (config.boardHeight - 1)
-  const lastCellColumn = "0" + (config.boardWidth - 1)
-  const expectedId = lastCellColumn.slice(-2) + lastCellRow.slice(-2)
+describe('Correctly build the game board', () => {
+  test('Builds board with correct number of cells', () => {
+    const expectedNumberOfCells = config.boardHeight * config.boardWidth
+    expect($tableCells.length).toBe(expectedNumberOfCells)
+  })
+  test('first cell has correct ID', () => {
+    const firstCellId = $tableCells.first().attr('id')
+    expect(firstCellId).toEqual('0000')
+  })
+  test('last cell has correct ID', () => {
+    const lastCellRow = "0" + (config.boardHeight - 1)
+    const lastCellColumn = "0" + (config.boardWidth - 1)
+    const expectedId = lastCellColumn.slice(-2) + lastCellRow.slice(-2)
 
-  const lastCellId = $tableCells.last().attr('id')
-  expect(lastCellId).toEqual(expectedId)
+    const lastCellId = $tableCells.last().attr('id')
+    expect(lastCellId).toEqual(expectedId)
+  })
 })
